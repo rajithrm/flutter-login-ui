e3bb122087e12cdb7422763e944b615e8ea38750
rajithrm <rajithrm.ibirix@gmail.com>
2026-03-18 23:51:55 +0530

    Added email and password form validation

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -8,6 +8,7 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
 
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
@@ -20,8 +21,14 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   void loginAction() {
-    print(emailController.text);
-    print(passwordController.text);
+    if (_formKey.currentState!.validate()) {
+      print(emailController.text);
+      print(passwordController.text);
+
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(const SnackBar(content: Text("Login Success (Demo)")));
+    }
   }
 
   @override
@@ -30,54 +37,89 @@ class _LoginScreenState extends State<LoginScreen> {
       backgroundColor: Colors.blueGrey,
       body: Padding(
         padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-
-            const Text(
-              "Login",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-
-            const SizedBox(height: 50),
-
-            TextField(
-              controller: emailController,
-              decoration: const InputDecoration(
-                labelText: "Email",
-                border: OutlineInputBorder(),
-              ),
-            ),
-
-            const SizedBox(height: 30),
-
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: const InputDecoration(
-                labelText: "Password",
-                border: OutlineInputBorder(),
-              ),
-            ),
-
-            const SizedBox(height: 70),
-
-            SizedBox(
-              width: 200,
-              child: ElevatedButton(
-                onPressed: loginAction,
-                child: const Text(
-                  "Login",
-                  style: TextStyle(color: Colors.black),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Login",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ),
 
-          ],
+              const SizedBox(height: 50),
+
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  labelText: "Email",
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter email";
+                  }
+
+                  final emailRegex = RegExp(
+                    r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+                  );
+
+                  if (!emailRegex.hasMatch(value)) {
+                    return "Enter valid email address";
+                  }
+
+                  return null;
+                },
+              ),
+
+              const SizedBox(height: 30),
+
+              TextFormField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: "Password",
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter password";
+                  }
+
+                  if (value.length < 6) {
+                    return "Password must be at least 6 characters";
+                  }
+
+                  if (!RegExp(r'[A-Z]').hasMatch(value)) {
+                    return "Must contain one uppercase letter";
+                  }
+
+                  if (!RegExp(r'[0-9]').hasMatch(value)) {
+                    return "Must contain one number";
+                  }
+
+                  return null;
+                },
+              ),
+
+              const SizedBox(height: 70),
+
+              SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: loginAction,
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
